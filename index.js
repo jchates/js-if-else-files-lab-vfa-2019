8002cce710027a020b994642d86195cea6e5fe3b
Jonah Chates <jonah.chates@gmail.com>
2019-06-14 15:30:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-var name = "Joe"
+var name = "Susan"
 var height = "74"
 
 
 // Don't worry about this
 module.exports = { name, height
 }
+var index = require("./index.js")
 
+if (name === "Susan") {
+  console.log("The name is correct")
+} else {
+  console.log("Expected the name to be Susan")
+}
